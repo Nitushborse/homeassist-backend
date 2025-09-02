2bb3923758cdd0d6134e2b3baf26c9b69437acc2
bhavesh <borsenitush@gmail.com>
2025-09-02 11:08:36 +0530

    created utility functions

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-
+import {app} from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => (console.log(`server is runing on port : ${process.env.PORT || 5000}`)))
+})
+.catch((err) => (console.log(`mongodb connection error: ${err}`)))
 
